19ac58f93835fe55b6093a4199056c56274fb840
Chucky <satotroy.ji@gmail.com>
2024-05-17 22:43:50 +0800

    add the table banner

--- src/components/main/lab.js
+++ src/components/main/lab.js
@@ -1,6 +1,71 @@
 import React from "react";
 import { Tab, Tabs } from "react-bootstrap";
 
+const label = (
+  collapseExample,
+  Name,
+  Language,
+  Difficulty,
+  Background,
+  href
+) => {
+  return (
+    <div className="container mt-2 mb-2">
+      <div className="row">
+        <div className="col-12">
+          <div
+            class="btn btn-block"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={"#" + collapseExample}
+            aria-expanded="false"
+            aria-controls={collapseExample}
+            style={{
+              width: "100%",
+              backgroundColor: "rgba(222,222,222,0.7)",
+            }}
+          >
+            <div className="row">
+              <div className="col-3" style={{ textAlign: "left" }}>
+                Project: {Name}
+              </div>
+              <div className="col-3" style={{ textAlign: "left" }}>
+                {Language}
+              </div>
+              <div className="col-3">Difficulty: {Difficulty}/10</div>
+              <div className="col-3" style={{ textAlign: "right" }}>
+                More Details ↓
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div class="collapse" id={collapseExample}>
+            <div
+              class="card card-body d-flex justify-content-between align-items-end"
+              style={{ textAlign: "left", lineHeight: 2 }}
+            >
+              <div>Background: {Background}</div>
+              <div style={{ width: "100px" }}>
+                <a
+                  className="btn btn-primary w-100"
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  More ...
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 function lab() {
   return (
     <div style={{ backgroundColor: "rgba(0,222,222,0.2)" }}>
@@ -20,54 +85,22 @@ function lab() {
           </Tab>
           <Tab eventKey="profile" title="Work Lab">
             <div style={{ margin: "10px" }}></div>
-            <div className="container mt-2 mb-2">
-              <div className="row">
-                <div className="col-12">
-                  <div
-                    class="btn btn-block"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseExample"
-                    aria-expanded="false"
-                    aria-controls="collapseExample"
-                    style={{
-                      width: "100%",
-                      backgroundColor: "rgba(222,222,222,0.7)",
-                    }}
-                  >
-                    <div className="row">
-                      <div className="col-4" style={{ textAlign: "left" }}>
-                        Project: Ad Units Generation
-                      </div>
-                      <div className="col-4">Difficulty: 2/10</div>
-                      <div className="col-4" style={{ textAlign: "right" }}>
-                        More Details ↓{" "}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-12">
-                  <div class="collapse" id="collapseExample">
-                    <div class="card card-body" style={{ textAlign: "left" }}>
-                      Background: In Google Ad Manager, whenever a new website
-                      is added, we need to include all ad units for each website
-                      according to Google Ads rules. This makes it easier for us
-                      to manage and track ad impressions.
-                      <a
-                        className="btn btn-primary w-100"
-                        href="/lab/worklab/adunit"
-                        target="_blank"
-                        style={{ width: "50px" }}
-                      >
-                        More ...
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {label(
+              "collapseExample",
+              "Ad Units Generation",
+              "Python, AWS, JavaScript",
+              2,
+              "In Google Ad Manager, whenever a new website is added, we need to include all ad units for each website according to Google Ads rules. This makes it easier for us to manage and track ad impressions.",
+              "/lab/worklab/adunit"
+            )}
+            {label(
+              "collapseExample1",
+              "Ad Units Generation",
+              "Python, AWS, JavaScript",
+              2,
+              "In Google Ad Manager, whenever a new website is added, we need to include all ad units for each website according to Google Ads rules. This makes it easier for us to manage and track ad impressions.",
+              "/lab/worklab/adunit"
+            )}
             <a
               className="btn btn-primary"
               style={{ margin: "10px" }}
